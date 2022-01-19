e48f1c8473e19a393512aefdffaf1c8914359530
FilipeMarx13 <54817989+FilipeMarx13@users.noreply.github.com>
2022-01-19 15:42:22 -0300

    Update to main

---
+++ DevFreela/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Api.Controllers
+{
+    [ApiController]
+    [Route("/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        public ProjectsController()
+        {
+        }
+    }
+}
